d68d2f00daccd756672ccc2713523aaf19ef857e
Vanya Artiukhov <K038486@eltham-college.org.uk>
2021-05-05 11:31:40 +0100

    Add project files.

---
+++ Tree/Binary_tree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    class Binary_tree
+    {
+        private Node root;
+
+        public Binary_tree(int n)
+        {
+            root = new Node(n);
+        }
+
+        public void add(int n)
+        {
+            Node currentnode = root;
+            bool Isempty = false;
+
+            while (Isempty == false)
+            {
+                if (currentnode < n)
+                {
+                    currentnode = currentnode.left;
+                }
+            }
+        }
+    }
+}
---
+++ Tree/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    class Node
+    {
+        public Node left;
+        public Node right;
+        public int element;
+
+        public Node(int n)
+        {
+            left = null;
+            right = null;
+            element = n;
+
+        }
+    }
+}
